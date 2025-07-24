94d3f01b911af246b331414dd676c80556e40a0d
Sybau <krystiankap39@gmail.com>
2025-07-24 16:49:58 +0200

    sigmuszka

--- script.js
+++ script.js
@@ -32,4 +32,11 @@
     }
   });
 
-
+window.addEventListener('scroll', () => {
+const header = document.querySelector('.site-header');
+if (window.scrollY > 10) {
+header.classList.add('scrolled');
+} else {
+header.classList.remove('scrolled');
+}
+});
